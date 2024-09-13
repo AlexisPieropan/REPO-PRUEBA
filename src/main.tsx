fd347b1fddff0edef822c435fde694aa0bf120da
AlexisPieropan <masterweb210@hotmail.com>
2024-09-13 00:26:06 -0300

    feat:se agrego componente arboles

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import App from './App.tsx'
 import './index.css'
-import  Beneficios  from './components/Beneficios/Beneficios.tsx'
+import  Arboles  from './components/Arboles/Arboles.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Beneficios />
+    <Arboles />
   </StrictMode>,
 )
