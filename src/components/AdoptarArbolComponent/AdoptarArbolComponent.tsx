f834681ba8b10ae7df5a3090d1d9b06b04eab16d
AlexisPieropan <masterweb210@hotmail.com>
2024-09-16 13:29:15 -0300

    feat: Se agrego componente AdoptarArbolComponent

---
+++ src/components/AdoptarArbolComponent/AdoptarArbolComponent.tsx
@@ -0,0 +1,23 @@
+import compraImage from '../../assets/compra.jpg'
+
+const AdoptarArbolComponent = ({ tree, onAdopt }: any) => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+      <img className="w-full h-48 object-cover" src={compraImage} alt="{tree.name}" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2 text-green-700">name</div>
+        <p className="text-gray-700 text-base">Tipo: </p>
+        <p className="text-gray-700 text-base">Ubicación: </p>
+        <p className="text-orange-700 font-semibold text-lg">$ precio / año</p>
+        <button
+          onClick={() => onAdopt(tree.id)}
+          className="mt-4 w-full bg-green-700 hover:bg-green-600 text-white font-bold py-2 px-4 rounded transition duration-200"
+        >
+          Adoptar este árbol
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AdoptarArbolComponent;
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 //import App from './App.tsx'
 import './index.css'
 import  Arboles  from './components/Arboles/Arboles.tsx'
+import  Bienvenidos  from './components/Bienvenidos/Bienvenidos.tsx'
+import AdoptarArbolComponent from './components/AdoptarArbolComponent/AdoptarArbolComponent.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Arboles />
+    <Bienvenidos />
+    <AdoptarArbolComponent />
   </StrictMode>,
 )
