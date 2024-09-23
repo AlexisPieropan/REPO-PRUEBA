4c39eca9bfa4a6f3193f3ab44f3cf9e8d7e64ca2
AlexisPieropan <masterweb210@hotmail.com>
2024-09-22 22:06:24 -0300

    feat:agrega componente Cosecha

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,10 @@ import  Arboles  from './components/Arboles/Arboles.tsx'
 import  Bienvenidos  from './components/Bienvenidos/Bienvenidos.tsx'
 import AdoptarArbolComponent from './components/AdoptarArbolComponent/AdoptarArbolComponent.tsx'
 import Adoptar from './pages/Adoptar/Adoptar.tsx'
+import CosechaComponent from './components/CosechaComponente/CosechaComponente.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Adoptar />
+    <CosechaComponent envioId={"2"} />
   </StrictMode>,
 )
